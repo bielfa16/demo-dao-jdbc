847b7e2aa1f2b6de66e9be50f8b43fe591853b8d
bielfa16 <gabriel_fa16@hotmail.com>
2025-04-13 11:26:03 -0300

    insert implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,5 +1,6 @@
 package model.dao.impl;
 
+import java.sql.Statement;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -25,8 +26,41 @@ public class SellerDaoJDBC implements SellerDao{
 	
 	@Override
 	public void insert(Seller obj) {
-		// TODO Auto-generated method stub
-		
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement(
+					"INSERT INTO seller "
+					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
+					+ "VALUES "
+					+ "(?, ?, ?, ?, ?)",
+					Statement.RETURN_GENERATED_KEYS);
+			st.setString(1, obj.getName());
+			st.setString(2, obj.getEmail());
+			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+			st.setDouble(4, obj.getBaseSalary());
+			st.setInt(5, obj.getDepartment().getId());
+			
+			int rowsAffected = st.executeUpdate();
+			
+			if (rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				if (rs.next()) {
+					int id = rs.getInt(1);
+					obj.setId(id);
+				}
+			
+			DB.closeResultSet(rs);
+			
+			}
+			else {
+				throw new DbException("Unexpected error! No rows affected");
+			}
+			
+		} catch (SQLException e ) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
@@ -55,7 +89,7 @@ public class SellerDaoJDBC implements SellerDao{
 				+ "ON seller.DepartmentId = department.Id "
 				+ "WHERE seller.Id = ?"); 	
 		
-		st.setInt(0, id);
+		st.setInt(1, id);
 		rs = st.executeQuery();
 			if (rs.next()) {
 				Department dep = instantiateDepartment(rs);
@@ -148,7 +182,7 @@ public class SellerDaoJDBC implements SellerDao{
 				+ "WHERE DepartmentId = ? "
 				+ "ORDER BY Name"); 	
 		
-		st.setInt(0, department.getId());
+		st.setInt(1, department.getId());
 		rs = st.executeQuery();
 		List<Seller> list = new ArrayList<>();
 		Map<Integer, Department> map = new HashMap<>();
